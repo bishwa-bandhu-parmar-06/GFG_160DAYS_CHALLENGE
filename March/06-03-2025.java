a9ca0476b7229d0c4728b92ef8d5d1d5f4e33fd9
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-03-06 23:09:59 +0530

    Longest Common Subsequence

---
+++ March/06-03-2025.java
@@ -0,0 +1,24 @@
+// Longest Common Subsequence
+// Difficulty: MediumAccuracy: 41.68%Submissions: 301K+Points: 4
+// Given two strings s1 and s2, return the length of their longest common subsequence (LCS). If there is no common subsequence, return 0.
+
+// A subsequence is a sequence that can be derived from the given string by deleting some or no elements without changing the order of the remaining elements. For example, "ABE" is a subsequence of "ABCDE".
+
+
+class Solution {
+    static int lcs(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
