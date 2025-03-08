63fa746942a390b4e111379b777c6d9ed25c6e4d
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-03-08 23:21:00 +0530

    Longest Palindrome in a String

---
+++ March/08-03-2025.java
@@ -0,0 +1,42 @@
+// Longest Palindrome in a String
+// Difficulty: MediumAccuracy: 23.2%Submissions: 322K+Points: 4
+// Given a string s, your task is to find the longest palindromic substring within s.
+
+// A substring is a contiguous sequence of characters within a string, defined as s[i...j] where 0 ≤ i ≤ j < len(s).
+
+// A palindrome is a string that reads the same forward and backward. More formally, s is a palindrome if reverse(s) == s.
+
+// Note: If there are multiple palindromic substrings with the same length, return the first occurrence of the longest palindromic substring from left to right.
+
+
+
+class Solution {
+    static String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        int start = 0, maxLength = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);     // Odd length palindrome
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
+            int len = Math.max(len1, len2);
+            
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        
+        return s.substring(start, start + maxLength);
+    }
+
+    static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
