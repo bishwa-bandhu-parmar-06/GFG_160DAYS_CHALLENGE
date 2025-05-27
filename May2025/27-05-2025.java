7144dc131523ff4638beb76f5c32535b33e636b0
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-05-27 19:34:51 +0530

    Print leaf nodes from preorder traversal of BST

---
+++ May2025/27-05-2025.java
@@ -0,0 +1,43 @@
+// Print leaf nodes from preorder traversal of BST
+// Difficulty: MediumAccuracy: 47.26%Submissions: 31K+Points: 4
+// Given a preorder traversal of a BST, find the leaf nodes of the tree without building the tree.
+
+
+
+
+
+
+class Solution {
+    
+    void func(int st, int end, int[] pre, ArrayList<Integer>res)
+    {
+        if(st==end)
+        {
+            res.add(pre[st]);
+            return;
+        }
+            
+        if(st>end)
+        return;
+        
+        int k = 0;
+        for(k = st; k<=end; k++)
+        {
+            if(pre[st]<pre[k])
+            break;
+        }
+        
+        func(st+1, k-1, pre, res);  //left subarray call
+        func(k, end, pre, res);  // right subarray call
+        
+    }
+    
+    public ArrayList<Integer> leafNodes(int[] pre) {
+        // code here
+        
+        ArrayList<Integer> res = new ArrayList<>();
+       
+       func(0, pre.length-1, pre, res);
+        return res;
+    }
+}
