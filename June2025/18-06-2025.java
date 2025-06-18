bb8e7df3f5e443527e3be2c0ebc540f2ec3ebbf8
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-06-18 07:48:17 +0530

    Find all possible palindromic partitions of a String

---
+++ June2025/18-06-2025.java
@@ -0,0 +1,34 @@
+// Find all possible palindromic partitions of a String
+// Difficulty: MediumAccuracy: 61.47%Submissions: 21K+Points: 4Average Time: 30m
+// Given a string s, find all possible ways to partition it such that every substring in the partition is a palindrome.
+
+
+class Solution {
+    public ArrayList<ArrayList<String>> palinParts(String s) {
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        backtrack(0, s, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, String s, ArrayList<String> current, ArrayList<ArrayList<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                current.add(s.substring(start, end + 1));
+                backtrack(end + 1, s, current, result);
+                current.remove(current.size() - 1);  
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
