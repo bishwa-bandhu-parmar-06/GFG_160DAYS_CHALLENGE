acaa806b24bd450286b2c885ba665d398a16ea3f
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-06-23 06:50:47 +0530

    Minimum sum

---
+++ June2025/23-06-2025.java
@@ -0,0 +1,39 @@
+// Minimum sum
+// Difficulty: MediumAccuracy: 17.14%Submissions: 150K+Points: 4
+// Given an array arr[ ] consisting of digits, your task is to form two numbers using all the digits such that their sum is minimized. Return the minimum possible sum as a string with no leading zeroes.
+
+
+
+class Solution {
+    String minSum(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        
+        StringBuilder num1 = new StringBuilder();
+        StringBuilder num2 = new StringBuilder();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) num1.append(arr[i]);
+            else num2.append(arr[i]);
+        }
+
+        return addStrings(num1.toString(), num2.toString());
+    }
+
+    String addStrings(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int digit1 = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int digit2 = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString().replaceFirst("^0+(?!$)", "");
+    }
+}
+
