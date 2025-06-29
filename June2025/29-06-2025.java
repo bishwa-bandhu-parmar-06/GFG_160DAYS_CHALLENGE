8dd98abdb55671c554963df497fa38e9f9d7bcbb
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-06-29 13:35:09 +0530

    Split Array Largest Sum

---
+++ June2025/29-06-2025.java
@@ -0,0 +1,55 @@
+// Split Array Largest Sum
+// Difficulty: HardAccuracy: 58.9%Submissions: 53K+Points: 8
+// Given an array arr[] and an integer k, divide the array into k contiguous subarrays such that the maximum sum among these subarrays is minimized. Find this minimum possible maximum sum.
+
+
+
+class Solution {
+    public int splitArray(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+            max = Math.max(max, arr[i]);
+        }
+        
+        int ans = sum;
+
+        int high = sum;  // if k=0, then the maximized minimum sum = sum of array
+        int low = max;  // minimum sum will be maximum element of the array.
+
+ 
+
+// Applying Binary Search
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isPossible(arr, k, mid)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    boolean isPossible(int[] arr, int k, int mid){
+        int count = 1;
+        int n = arr.length;
+        int sum = 0;
+        
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+            if(sum>mid){
+                sum = arr[i];
+                count++;
+            }
+        }
+        if(count<=k){
+            return true;
+        }
+        return false;
+    }
+}
