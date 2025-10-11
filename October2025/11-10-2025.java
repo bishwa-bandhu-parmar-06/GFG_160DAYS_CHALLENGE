bf5537b6304b5f971557f4ebf8db334213ff1506
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-10-11 18:11:27 +0530

    Maximum path sum

---
+++ October2025/11-10-2025.java
@@ -0,0 +1,25 @@
+// Maximum path sum
+// Difficulty: MediumAccuracy: 42.92%Submissions: 110K+Points: 4Average Time: 45m
+// Given the root of a binary tree, your task is to find the maximum path sum. The path may start and end at any node in the tree.
+
+class Solution {
+    long max, min = Integer.MIN_VALUE;
+    int findMaxSum(Node root) {
+        // code here
+        max = (long)Integer.MIN_VALUE;
+        findMaxSum2(root);
+        return (int)max;
+    }
+    long findMaxSum2(Node root){
+        if(root == null){
+            return min;
+        }
+        long left = findMaxSum2(root.left);
+        long right = findMaxSum2(root.right);
+        
+        long max2 = Math.max((long)root.data,Math.max((long)root.data+left,(long)root.data+right));
+        max = Math.max(max,Math.max(max2,Math.max(left,Math.max(right,left+right+(long)root.data))));
+    
+        return max2;
+    }
+}
