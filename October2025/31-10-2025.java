14e877723ae652635c382d8456f078b06775fd8a
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-10-31 17:44:24 +0530

    3289. The Two Sneaky Numbers of Digitville

---
+++ October2025/31-10-2025.java
@@ -0,0 +1,19 @@
+
+
+
+// 3289. The Two Sneaky Numbers of Digitville
+
+
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int n = nums.length;
+        boolean[] a = new boolean[n];
+        int[] ar = new int[2];
+        int k = 0;
+        for(int i = 0;i<n;i++){
+            if(a[nums[i]]==true) ar[k++] = nums[i];
+            else a[nums[i]] = true;
+        }
+        return ar;
+    }
+}
