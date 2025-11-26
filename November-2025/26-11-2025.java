d50525fa2759f751f0380f533fa442817006b151
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-11-26 07:21:18 +0530

    AND In Range

--- November-2025/26-11-2025.java
+++ November-2025/26-11-2025.java
@@ -5,50 +5,31 @@
 
 
 class Solution {
-    
-    int cnt(int n){
-        
-        int ans=0;
-        while(n>0){
-            
-            n=n/2;
-            ans++;
-        }
-        
-        return ans;
-    }
-  public:
-    int andInRange(int l, int r) {
-    
 
-    int n=cnt(l);
-    
-  int m=cnt(r);
-  
-  if(n!=m){
-      return 0;
-  }
-  
-  int ans=0;
-  
- for(int i=cnt(l);i>=0;i--){
-     
-     int left=((l>>i)&1);
-     int right= ((r>>i)&1);
-     
-     if(left!=right){
-     break;
-     }
-    
-else if(left==1){
-    
-    ans=ans+(1<<i);
-}
-     
- }
+    public int andInRange(int l, int r) {
+
+        int shift = 0;
+
  
 
-  return ans;
-        
+        // find common prefix
+
+        while (l < r) {
+
+            l >>= 1;
+
+            r >>= 1;
+
+            shift++;
+
+        }
+
+ 
+
+        // restore shifted bits
+
+        return l << shift;
+
     }
-};
+
+}
