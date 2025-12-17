5bbe43e4c351de060d2d3a720938834cabc2115a
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-12-17 20:59:46 +0530

    Overlapping Intervals

---
+++ December_2025/17-12-2025.java
@@ -0,0 +1,33 @@
+// Overlapping Intervals
+// Difficulty: MediumAccuracy: 57.41%Submissions: 122K+Points: 4Average Time: 20m
+// Given an array of Intervals arr[][], where arr[i] = [starti, endi]. The task is to merge all of the overlapping Intervals.
+
+
+class Solution {
+    public ArrayList<int[]> mergeOverlap(int[][] arr) {
+        // Code here
+        Arrays.sort(arr,new Comparator<int[]>(){
+            public int compare(int[] arr1,int[] arr2){
+                return Integer.compare(arr1[0],arr2[0]);
+            }
+        });
+        ArrayList<int[]> result=new ArrayList<>();
+        int n=arr.length;
+        int i=0;
+        int j=1;
+        while(j<n){
+            if(arr[j][0]<=arr[i][1]){
+                arr[i][1]=Math.max(arr[i][1],arr[j][1]);
+                j++;
+            }
+            else{
+                result.add(arr[i]);
+                i=j;
+                j++;
+            }
+        }
+        result.add(arr[i]);
+        return result;
+    }
+}
+
