674783175796c197905b53507df018a2ff9bcc5b
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-01-02 21:16:19 +0530

    Sort 0s, 1s and 2s

---
+++ January-2026/02-01-2026.java
@@ -0,0 +1,13 @@
+// Sort 0s, 1s and 2s
+// Difficulty: MediumAccuracy: 50.58%Submissions: 833K+Points: 4Average Time: 10m
+// Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+// Note: You need to solve this problem without utilizing the built-in sort function.
+
+
+
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
