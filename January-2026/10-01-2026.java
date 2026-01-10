db9cc993534a519228dc5e34ebf40f6d81f461a4
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-01-10 20:10:43 +0530

    Substrings with K Distinct

---
+++ January-2026/10-01-2026.java
@@ -0,0 +1,32 @@
+// Substrings with K Distinct
+// Difficulty: MediumAccuracy: 20.46%Submissions: 186K+Points: 4Average Time: 20m
+// You are given a string s consisting of lowercase characters and an integer k, You have to count all possible substrings that have exactly k distinct characters.
+
+
+class Solution {
+    public int atMost(String s, int k) {
+        // code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = 0, r = 0, cnt = 0;
+        int n = s.length();
+
+        while (l < n) {
+            map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0) + 1);
+
+            while (map.size() > k) {
+                map.put(s.charAt(r), map.get(s.charAt(r)) - 1);
+                if (map.get(s.charAt(r)) == 0)
+                    map.remove(s.charAt(r));
+                r++;
+            }
+
+            cnt += l - r + 1;
+            l++;
+        }
+        return cnt;
+    }
+    public int countSubstr(String s, int k){
+        return atMost(s,k)- atMost(s,k-1);
+    }
+    
+}
