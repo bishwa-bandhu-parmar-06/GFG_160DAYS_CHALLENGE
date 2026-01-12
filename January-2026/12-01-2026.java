26e766c1508fc5689f32b067df0379b5c7ef3438
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-01-12 21:15:48 +0530

    K Sized Subarray Maximum

---
+++ January-2026/12-01-2026.java
@@ -0,0 +1,35 @@
+// K Sized Subarray Maximum
+// Difficulty: MediumAccuracy: 26.04%Submissions: 423K+Points: 4
+// Given an array arr[] of positive integers and an integer k. You have to find the maximum value for each contiguous subarray of size k. Return an array of maximum values corresponding to each contiguous subarray.
+
+
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        int max=0;
+        ArrayList<Integer> list=new ArrayList<>();   
+        for(int i=0;i<k;i++)
+        {
+            if(max<arr[i])
+                max=arr[i];
+        }
+        list.add(max);
+        int len=k;
+        for(int i=0;i<arr.length-len;i++)
+        {
+            if(arr[i]==max)
+            {
+                max=0;
+                for(int j=i+1;j<=k;j++)
+                {
+                    if(max<arr[j])
+                        max=arr[j];
+                }
+            }
+            if(arr[k]>max)
+                max=arr[k];
+            list.add(max);
+            k++;
+        }
+        return list;
+    }
+}
